e5a017f64a67398221c13698c9de2346eeb06e91
Marixxa24 <106198154+Marixxa24@users.noreply.github.com>
2023-09-04 13:30:18 -0300

    button chatbot

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
-Toastify({
-    text: "This is a toast",
-    duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
-    newWindow: true,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "left", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-    onClick: function(){} // Callback after click
-  }).showToast();
+/* de esta manera no funciona 
+  document.addEventListener('DOMContentLoaded', function () {
+    const openChatButton = document.getElementById('open-chat');
+    const closeChatButton = document.getElementById('close-chat');
+    const chatbot = document.getElementById('chatbot');
+
+    // Función para abrir la ventana de chat
+    openChatButton.addEventListener('onclick', function () {
+      chatbot.style.display = 'block';
+  });
+
+    // Función para cerrar la ventana de chat
+    closeChatButton.addEventListener('click', function () {
+      chatbot.style.display = 'none';
+  });
+
+    // Aquí puedes agregar la lógica para enviar mensajes y recibir respuestas del chatbot
+    const messageInput = document.getElementById('message-input');
+    const sendMessageButton = document.getElementById('send-message');
+
+    sendMessageButton.addEventListener('click', function () {
+        const messageText = messageInput.value.trim();
+        if (messageText !== '') {
+            // Agrega aquí la lógica para enviar el mensaje al chatbot y mostrar la respuesta.
+            // Por ejemplo, puedes agregar la respuesta en la sección de "chatbot-messages".
+            const chatbotMessages = document.querySelector('.chatbot-messages');
+            const userMessage = document.createElement('div');
+            userMessage.className = 'chatbot-message user';
+            userMessage.textContent = messageText;
+            chatbotMessages.appendChild(userMessage);
+
+            // Aquí debes enviar el mensaje al chatbot y obtener la respuesta del chatbot
+            // Luego, puedes agregar la respuesta del chatbot como un elemento '.chatbot-message bot' en chatbotMessages
+            // Por simplicidad, este ejemplo solo muestra el mensaje del usuario.
+            messageInput.value = '';
+        }
+    });
+});
+
+
+*/
+// script.js
+
+// Función para abrir el modal
+function openChat() {
+  var modal = document.getElementById("chatbot");
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function closeChat() {
+  var modal = document.getElementById("chatbot");
+  modal.style.display = "none";
+}
